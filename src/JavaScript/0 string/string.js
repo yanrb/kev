7425a146309cda975c9bc4cf835a6348f9930fd5
Bryan Perez <bryan4971@gmail.com>
2019-12-31 14:29:47 -0800

    added more problems and test

--- src/JavaScript/0 string/string.js
+++ src/JavaScript/0 string/string.js
@@ -1,4 +1,12 @@
 /*
+
+   RESOURCES:
+      https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Strings
+      https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Useful_string_methods
+
+ */
+
+/*
    Given a string, replace all occurrence of "x" with "e"
 
    EXAMPLE #1:
@@ -6,35 +14,74 @@
       OUTPUT   "hello there kevin"
  */
 
- /*
-export const replaceAllXChars = (str) => {
-   return "TO DO";
-};
-*/
-
-//For loop
-
 export const replaceAllXChars = (str) => {
 
    let changeX = "";
-   for(let i = 0 ; i < str.length; i++){
-      if(str.charAt(i) === 'x'){
-      changeX += 'e';
-   }else{
-      changeX += str.charAt(i);
-   };
+   for (let i = 0; i < str.length; i++) {
+      if (str.charAt(i) === 'x') {
+         changeX += 'e';
+      } else {
+         changeX += str.charAt(i);
+      }
+   }
 
+   return changeX;
 };
-  
-return changeX;
-};
-
 
 
 /*
-//Using replace()
+   Determine if a string has all unique characters
 
-export const replaceAllXChars = (str) => {
-   return str.replace(/x/g, 'e');
-}
-*/
+   Return TRUE if string input has all unique characters. Otherwise, return FALSE
+
+   EXAMPLE:
+      INPUT    abc
+      OUTPUT   true
+ */
+export const hasUniqueCharacters = (str) => {
+   // === CODE HERE ===
+};
+
+
+/*
+   Determine if a string is a palindrome
+   Return TRUE if it is. Otherwise, return FALSE
+
+   Palindrome: a word, phrase, or sequence that reads the same backward as forward
+      e.g., "madam" or "nurses run"
+ */
+export const isPalindrome = (str) => {
+   // === CODE HERE ===
+};
+
+
+/*
+   DIFFICULTY: Challenging
+
+   Given a string, format it such that it only contains a number and it is formatted with two
+   decimal places and has a leading "$"
+
+   EXAMPLE #1:
+      INPUT       "12345"
+      OUTPUT      "$123.45"
+
+   EXAMPLE #2:
+      INPUT       "123asd"
+      OUTPUT      "$1.23"
+
+   EXAMPLE #3:
+      INPUT       ""
+      OUTPUT      "$0.00"
+
+   EXAMPLE #4:
+      INPUT       "0000040"
+      OUTPUT      "$0.040"
+
+   EXAMPLE #5:
+      INPUT       "#0000001asldk2nd()3"
+      OUTPUT      "$1.23"
+
+ */
+export const formatToCurrency = (str) => {
+   // === CODE HERE ===
+};
--- src/JavaScript/0 string/string.test.js
+++ src/JavaScript/0 string/string.test.js
@@ -1,9 +1,82 @@
-import {replaceAllXChars} from "./string"
+import {
+   replaceAllXChars,
+   hasUniqueCharacters,
+   isPalindrome,
+   formatToCurrency
+} from "./string"
 
 test('TEST #1: replaceAllXChars', () => {
    expect(replaceAllXChars("hxllo therx kxvin")).toBe("hello there kevin");
 });
 
-test('TEST #1: replaceAllXChars', () => {
-   expect(replaceAllXChars("hxllo therx kxvin")).toBe("hello there kevin");
+
+test('TEST #1: hasUniqueCharacters', () => {
+   expect(hasUniqueCharacters("asdfg")).toBe(true);
+});
+
+test('TEST #2: hasUniqueCharacters', () => {
+   expect(hasUniqueCharacters("a%a")).toBe(false);
+});
+
+test('TEST #3: hasUniqueCharacters', () => {
+   expect(hasUniqueCharacters("asdfgasdwqeqweqw")).toBe(false);
+});
+
+test('TEST #4: hasUniqueCharacters', () => {
+   expect(hasUniqueCharacters("qwertyuiopmnbvcxz!@#$5")).toBe(true);
+});
+
+// ===== isPalindrome
+
+test('TEST #1: isPalindrome', () => {
+   expect(isPalindrome("asa")).toBe(true);
+});
+
+test('TEST #2: isPalindrome', () => {
+   expect(isPalindrome("a%a")).toBe(true);
+});
+
+test('TEST #3: isPalindrome', () => {
+   expect(isPalindrome("asdfgasdwqeqweqw")).toBe(false);
+});
+
+test('TEST #4: isPalindrome', () => {
+   expect(isPalindrome("moom")).toBe(true);
+});
+
+// ===== formatToCurrency =====
+test('TEST #0: formatToCurrency', () => {
+   expect(formatToCurrency("1")).toBe("$0.01");
+});
+
+test('TEST #0.a: formatToCurrency', () => {
+   expect(formatToCurrency("10")).toBe("$0.10");
+});
+
+test('TEST #0.b: formatToCurrency', () => {
+   expect(formatToCurrency("100")).toBe("$1.00");
+});
+
+test('TEST #0.c: formatToCurrency', () => {
+   expect(formatToCurrency("100000")).toBe("$1,000.00");
+});
+
+test('TEST #1: formatToCurrency', () => {
+   expect(formatToCurrency("123asd")).toBe("$1.23");
+});
+
+test('TEST #2: isPalindrome', () => {
+   expect(formatToCurrency("")).toBe("$0.00");
+});
+
+test('TEST #3: formatToCurrency', () => {
+   expect(formatToCurrency("0000040")).toBe("$0.040");
+});
+
+test('TEST #4: formatToCurrency', () => {
+   expect(formatToCurrency("#0000001asldk2nd()3")).toBe("$1.23");
+});
+
+test('TEST #5: formatToCurrency', () => {
+   expect(formatToCurrency("#0000001asldk2nd()31234")).toBe("$12,312.34");
 });
