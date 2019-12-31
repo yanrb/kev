97b2878fb8db9d1017a882ae700110946be1ea6f
Bryan Perez <bryan4971@gmail.com>
2019-12-30 18:21:44 -0800

    first commit

---
+++ src/JavaScript/0 string/string.js
@@ -0,0 +1,10 @@
+/*
+   Given a string, replace all occurrence of "x" with "e"
+
+   EXAMPLE #1:
+      INPUT    "hxllo therx kxvin"
+      OUTPUT   "hello there kevin"
+ */
+export const replaceAllXChars = (str) => {
+   return "TO DO";
+};
---
+++ src/JavaScript/0 string/string.test.js
@@ -0,0 +1,9 @@
+import {replaceAllXChars} from "./string"
+
+test('TEST #1: replaceAllXChars', () => {
+   expect(replaceAllXChars("hxllo therx kxvin")).toBe("hello there kevin");
+});
+
+test('TEST #1: replaceAllXChars', () => {
+   expect(replaceAllXChars("hxllo therx kxvin")).toBe("hello there kevin");
+});
