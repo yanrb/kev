e505ac643bcc74bb442c8dad16e8cf1156033149
Bryan Perez <bryan4971@gmail.com>
2020-01-03 22:37:20 -0800

    added array, queue and stack problems

---
+++ src/JavaScript/1 array/array.js
@@ -0,0 +1,54 @@
+/*
+      Return all of the prime numbers that are less than the argument
+
+      EXAMPLE:
+         INPUT    10
+         OUTPUT   [1, 2, 3, 5, 7]
+ */
+
+export const getPrimesUpTo = (max) => {
+  // ===== your code here =====
+   return [];
+};
+
+/*
+
+      Given an array arr[] of N integers, the task is to find the
+      largest subset of arr[] such that in every pair of numbers
+      from that subset, one number is a divisor of the other.
+
+      EXAMPLE:
+         INPUT    arr= [1, 2, 3, 4, 5]
+         OUTPUT   [1, 2, 4]
+
+
+      the "%" is called the 'modulo' operator
+         the modulo operation finds the remainder
+         after division of one number by another
+
+         4 % 2 = 0
+         4 % 1 = 0
+         2 % 1 = 0
+
+ */
+
+export const getLargestDivisorSubset = (arr) => {
+   // ===== your code here =====
+   return [];
+};
+
+
+/*
+      Given a 2-dimensional array, find the maximum sum of columns
+
+      EXAMPLE:
+         INPUT
+            [[1, 2, 3],]
+            [[4, 5, 6],]
+            [[7, 8, 9]]
+
+         OUTPUT  9 + 6 + 3 = 18
+ */
+export const maxSumCol = (twoDimArr) => {
+
+};
---
+++ src/JavaScript/2 class/Stack.js
@@ -0,0 +1,50 @@
+// SEE: https://www.geeksforgeeks.org/stack-data-structure-introduction-program/
+// You can watch youtube videos explaining the stack data structure
+// The idea should be universal regardless of the language
+
+/*
+
+      Implement a Stack data structure
+ */
+class Stack {
+
+   /*
+      YOUR CODE HERE
+    */
+
+   //    Where the size is the maximum number of elements that your stack can have
+   constructor(size) {
+
+   }
+
+   // Adds an item in the stack. If the stack is full, then it is said to be an Overflow condition.
+   push(item) {
+
+   }
+
+   /*
+         Removes and returns the "top" item of the stack
+         The items are popped in the reversed order in which they are pushed.
+
+         If the stack is empty, then it is said to be an Underflow condition.
+         In this case, return undefined and keep stack empty
+    */
+   pop() {
+
+   }
+
+   /*
+         Returns top element of stack.
+    */
+   peek() {
+
+   }
+
+   /*
+         Returns true if stack is empty, else false.
+    */
+   isEmpty() {
+
+   }
+
+}
