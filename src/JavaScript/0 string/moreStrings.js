aba8289b2090b7a0aa6c017c090bb4caec735152
xkevinpham <57520301+xkevinpham@users.noreply.github.com>
2020-01-07 09:20:36 -0800

    Final version
    Completed version of passing all test to determine if one or two edits away.

--- src/JavaScript/0 string/moreStrings.js
+++ src/JavaScript/0 string/moreStrings.js
@@ -29,48 +29,42 @@
 
 export const isOneOrZeroEditAway = (str1, str2) => {
 
-   
    let length1 = str1.length;
    let length2 = str2.length;
-
+   let length = Math.abs(length1 - length2);
+   
    //If both strings are same = zero edits return true
    if(str1 === str2){
       return true;
    }
-   //If both length is greater than 1 = two edits made returns false
-   if((length1 - length2) > 1 ){
-      return false;
-   }
-
+ 
    //Checks each characters and count how many indexes needs changes
-   
    let difference = 0;
-   for(let i = 0; i < length1; i++){
-      for(let x = 0; x < length2; x++){
+   for(let i = 0; i < length1;){
+      for(let x = 0; x < length2;){
          if(str1[i] != str2[x]){
             difference++;
-            }else if(difference > 1){
-               return false;
-            }else{
-               return true;
+            i++;
+            x++;
+            }else if(str1[i] == str2[x]){
+               i++;
+               x++;
+            //Checks the two length of both string if needs to add a missing index or not
+            }else if(length == 1){
+               if(lenght1 > length2){
+                  x++;
+                }else{
+                  i++;
+            }
+            difference++;
          }
-       }
-   }
-   
-      /*
-      else if(difference > 1){
-         return false;
-      }else{
-         return true;
       }
+   //Checks how many differences and returns the correct output if greater than 1 edit
+   if(difference > 1){
+      return false;
+   }else{
+      return true;
    }
-*/
-
-/*
-  if(difference > 1){
-     return false;
-  }
-*/
-
+}  
 
 };
