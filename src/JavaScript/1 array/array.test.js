08e90ec6ba6cc70868469d642a987014f0d575c8
xkevinpham <57520301+xkevinpham@users.noreply.github.com>
2020-01-07 21:32:47 -0800

    add array test, first draft solving
    first draft solving getPrimesUpTo,  Having trouble filter out even numbers.

---
+++ src/JavaScript/1 array/array.test.js
@@ -0,0 +1,10 @@
+import {
+    getPrimesUpTo,
+    getLargestDivisorSubset,
+    maxSumCol
+} from "./array"
+
+test('TEST #1: getPrimesUpTo', () => {
+    expect(getPrimesUpTo("10")).toBe("[1, 2, 3, 5, 7]");
+ });
+
