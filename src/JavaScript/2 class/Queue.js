5a9cecb831e782a26b71dc077cc53890957b2db6
Bryan Perez <bryan4971@gmail.com>
2020-01-14 10:22:02 -0800

    new array problems added

--- src/JavaScript/2 class/Queue.js
+++ src/JavaScript/2 class/Queue.js
@@ -1,1 +1,72 @@
-// similar to stack
+// SEE: https://www.geeksforgeeks.org/queue-data-structure/
+// Another resource: https://www.tutorialspoint.com/data_structures_algorithms/dsa_queue.htm
+// You can watch youtube videos explaining the queue data structure
+// The idea should be universal regardless of the language
+
+
+// I think of a queue like a line in a bank or cashier
+
+/*
+      Implement a Queue data structure
+ */
+class Queue {
+
+
+   //    Where the size is the maximum number of elements that your queue can have
+   constructor(size) {
+
+   }
+
+   /*
+         adds item to the queue (where do you put the item on the queue)
+    */
+   enqueue(item) {
+
+   }
+
+   /*
+         remove an item from the queue (what item do you remove first in a queue?)
+         and returns this item.
+
+         Return undefined if empty
+    */
+   dequeue(){
+
+   }
+
+
+   /*
+         Gets the element at the front of the queue without removing it.
+         Return undefined if empty
+    */
+   peek(){
+
+   }
+
+   /*
+         Check if the queue is full
+
+         Returns TRUE if full. Otherwise, returns false
+    */
+   isFull() {
+
+   }
+
+
+   /*
+         Checks if queue is empty
+         Return TRUE if empty. Otherwise, return false
+    */
+   isEmpty() {
+
+   }
+
+   /*
+         Returns the max capacity of elements of the queue
+    */
+   getSize() {
+
+   }
+}
+
+export default Queue;
