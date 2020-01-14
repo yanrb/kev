0cf9183bb4bd36ef8935024d762a5bb5684aa05d
xkevinpham <57520301+xkevinpham@users.noreply.github.com>
2020-01-14 10:15:38 -0800

    sample queue stack ?
    trying to draft a sample version of a queue stack

--- src/JavaScript/2 class/Queue.js
+++ src/JavaScript/2 class/Queue.js
@@ -1,1 +1,37 @@
 // similar to stack
+
+class Queue{
+    constructor(){
+        this.queueLine = []
+        this.first = 0
+        this.last = 0
+    }
+
+    enqueue(element){
+        this.queueLine[this.last] = element
+        this.last++
+    }
+
+    dequeue(){
+        let removed = this.queueLine[this.first]
+        delete this.queueLine[this.first]
+        this.first++
+        return removed
+    }
+
+    peek(){
+        if(this.isEmpty()){
+            return "Queue is empty"
+        }else{
+            return this.queueLine[0]
+        }
+    }
+
+    isEmpty(){
+        if(this.queueLine.length === 0){
+            return true
+        }else{
+            return false
+        }
+    }
+}
